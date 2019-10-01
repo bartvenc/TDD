474f7bf1867609d1eda449b63e3fb9280b98be21
bartvenc <pheonexlt@gmail.com>
2019-10-01 19:23:51 +0200

    Expermented with Tower update and targetEnemy functioons

--- src/Tower.cpp
+++ src/Tower.cpp
@@ -1,5 +1,6 @@
 #include "Tower.hpp"
 #include <math.h> 
+//#include "Timer.hpp"
 
 Tower::Tower(Manager* man) : manager(man){
 
@@ -7,16 +8,45 @@ Tower::Tower(Manager* man) : manager(man){
 
 Tower::~Tower(){}
 
+
+
 void Tower::addTower(float x, float y){
 	auto& tower(manager->addEntity());
 	tower.addComponent<TransformComponent>(x, y, 64, 64,1);
 	tower.addComponent<SpriteComponent>("assets/T1.png", false);
-	tower.addComponent<CircleComponent>(x,y,200);
+	tower.addComponent<CircleComponent>(x,y,128);
 	//tower.addComponent<ColliderComponent>("tower");
 	tower.addGroup(Game::groupTowers);	
 	t = &tower.getComponent<TransformComponent>();
 	s = &tower.getComponent<SpriteComponent>();	
+	hitList = false;
+	sTimer = new Timer();
 }
+
+void Tower::targetEnemy(Enemy *enemy){
+
+	Vector2D targetPos = enemy->t->position;
+	Vector2D targetVel = enemy->t->velocity;
+	Vector2D totarget = (targetPos + targetVel*25) - t->position;
+	//Vector2D trackTarget = pos - t->position;
+
+	//trackTarget = trackTarget.Normalize();
+	enemyPosx = totarget.Normalize();
+
+	s->angle= (atan2(totarget.x, -totarget.y)*180/3.14159);
+
+	enemyPosx*7;
+	enemys = enemy;
+	printf("Enemys.health %d\n", enemys->health);
+	//objects.emplace(enemy, enemyPosx);
+	//printf("enemy pos: ");
+	//std::cout << enemy->t->position << std::endl;
+	//printf("enemyPosx:");
+	//	std::cout << enemyPosx << std::endl;
+	//hasTarget = true;
+}
+
+
 
 void Tower::targetEnemy(Vector2D pos, Vector2D vel){
 
@@ -45,29 +75,67 @@ void Tower::targetEnemy(Vector2D pos, Vector2D vel){
 	//std::cout<<totarget<<std::endl;
 
 	enemyPos*7;
-
+	//printf("Enemys.health %d\n", enemys->health);
 	//printf("pos: ");
 	//std::cout<<pos<<std::endl;
 
 	//
-	hasTarget = true;
+	//hasTarget = true;
 }
 
-void Tower::update() {
-	int currentTime = SDL_GetTicks();
-	//printf("%s","tower update enemyPos" );
-	//printf("(currentTime % 2000)= %d\n",(currentTime % 5000) );
-	//std::cout<<enemyPos<<std::endl;
-	if((currentTime % 1000) < 10  && hasTarget){
+void Tower::update(int h) {
+
+
+	if(enemys != NULL && hitList){
+		printf("hasTarget pos: ");
+		std::cout << enemys->t->position << std::endl;
+		hasTarget = true;
+	}else{
+		hasTarget = false;
+	}
+
+	printf("Tower Update.---\n");
+	printf("hitList: %d, hasTarget %d \n", hitList,hasTarget);
+
+
+
+
+	sTimer->update();
+	if((sTimer->deltaTime() >= 2 ) && hasTarget){
 		
 		printf("Creating projectile with angle: %f\n", s->angle );
 		auto& projectile(manager->addEntity());
 		projectile.addComponent<TransformComponent>(t->position.x, t->position.y, 35, 35, 1);
 		projectile.addComponent<SpriteComponent>("assets/Bullet.png", false);
-		projectile.addComponent<ProjectileComponent>(800, 7, enemyPos);
+		projectile.addComponent<ProjectileComponent>(800, 7, enemyPosx);
 		projectile.addComponent<CircleComponent>(t->position.x, t->position.y,50);
 		projectile.addComponent<ColliderComponent>("projectile");
 		projectile.addGroup(Game::groupProjectiles);
+		sTimer->reset();
+		hasTarget = false;
+		
 	}
 	hasTarget = false;
+	
+	
 }
+	//int currentTime = SDL_GetTicks();
+	//printf("%s","tower update enemyPos" );
+	//printf("(currentTime % 2000)= %d\n",(currentTime % 5000) );
+	//std::cout<<enemyPos<<std::endl;
+	/*printf("HitList %d =\n", hitList);
+	if(hitList < objects.size()){
+		for(int i = 0; i < hitList; i++){
+			objects.erase(0);
+		}
+	}
+	printf("objects.size() %d \n", objects.size());
+	std::map<Enemy*, Vector2D>::iterator ems = objects.begin();
+	/*while(ems != objects.end()){
+
+		if(ems->first != NULL){
+			hasTarget = true;
+			enemyPos = ems->second;
+		}	
+
+	}*/
--- src/Tower.hpp
+++ src/Tower.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#include "Game.hpp"
 #include "ECS/Components.hpp"
+#include "Enemy.hpp"
+#include <boost/variant.hpp>
+#include <map>
 
 class Tower{
 private:
@@ -8,16 +12,26 @@ public:
 
 	Tower(Manager* man);
 	~Tower();
+
 	TransformComponent* t;
 	SpriteComponent* s;
 	CircleComponent * range;
+	//std::vector<Enemy*, Vector2D> enemiesInRange;
+	std::map<Enemy*, Vector2D> objects;
+	Enemy *enemys;
 	void init();
-	void update();
+	void update(int h);
 	void draw();
 	void addTower(float x, float y);
+	void targetEnemy(Enemy *enemy);
 	void targetEnemy(Vector2D pos, Vector2D vel);
+
 	Vector2D enemyPos;
+	Vector2D enemyPosx;
 	bool hasTarget;
+	bool hitList;
+	Timer* sTimer;
+
 
 
 };
