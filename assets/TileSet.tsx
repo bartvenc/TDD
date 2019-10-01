7fee741b19e49b44692d56640fad89958c2097d6
bartvenc <pheonexlt@gmail.com>
2019-10-01 19:24:39 +0200

    Added new Assets

---
+++ assets/TileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="TileSet" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="TileSet.png" width="640" height="640"/>
+</tileset>
